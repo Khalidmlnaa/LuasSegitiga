176301bb9909421087b0cd8bde208530b6a8323f
Khalidmlnaa <114723568+Khalidmlnaa@users.noreply.github.com>
2022-09-30 14:33:09 +0800

    Add files via upload

---
+++ LuasSegitiga.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipedata;
+
+/**
+ *
+ * @author User
+ */
+public class LuasSegitiga {
+    public static void main(String[] args) {
+        int alas= 1;
+        int tinggi= 8;
+        double luas= (double)alas*tinggi/2;
+        
+        System.out.println("Luas Segitiga : "+luas);
+    }
+    
+}
